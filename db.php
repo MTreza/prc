a2b9019353717b79a8671b9989041a90c6d13e60
reza kiani <reza3830@gmail.com>
2023-05-24 12:47:31 +0330

    add class & rigester

--- db.php
+++ db.php
@@ -7,7 +7,7 @@ $dbname = "login";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
-    die("connection failed:" . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 return $conn;
 ?>
